b1f6dd60d42b871f0a0ac4613b4bde078602251c
tcarvalho <tcarvalho@grandev.com.br>
2015-02-18 21:08:37 -0300

    Correção no sql da tabela altera_senha

--- etc/db/altera_senha.sql
+++ etc/db/altera_senha.sql
@@ -1,14 +1,13 @@
 -- Table: altera_senha
 
--- DROP TABLE altera_senhal;
+-- DROP TABLE altera_senha;
 
 CREATE TABLE altera_senha
 (
   codigo character varying(16) NOT NULL,
-  email_antigo character varying(45),
-  email_novo character varying(45),
-  status character varying(1),
-  CONSTRAINT altera_email_pkey PRIMARY KEY (codigo)
+  email character varying(45),
+  status character varying(45),
+  CONSTRAINT altera_senha_pkey PRIMARY KEY (codigo)
 )
 WITH (
   OIDS=FALSE
